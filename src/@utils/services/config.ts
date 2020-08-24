a083ecbf3d45222833d60a16cce7724786981c1b
Warner <934797303@qq.com>
2020-08-24 14:13:44 +0800

    ConfigService Singleton

--- src/@utils/services/config.ts
+++ src/@utils/services/config.ts
@@ -5,20 +5,26 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ConfigService {
   static instance: any;
-  private envConfig: dotenv.DotenvParseOutput;
-  constructor() {
-    this.envConfig = dotenv.parse(fs.readFileSync('.env'));
-  }
+  private envConfig: object;
+  private constructor() { }
 
-  static getInstance() {
+  /**
+   * 获取唯一实例
+   */
+  static getInstance(): ConfigService {
     if (!this.instance) {
       this.instance = new ConfigService;
     }
     return this.instance;
   }
 
-  // Docker 变量或系统变量覆盖 env 配置文件
-  get(key: string) {
+  /**
+   * 获取单个配置
+   * 原则： Docker 变量或系统变量覆盖 env 配置文件
+   * @param key 配置 key
+   */
+  get(key: string): any {
+    this.envConfig = dotenv.parse(fs.readFileSync('.env'));
     return process.env[key] || this.envConfig[key];
   }
 
